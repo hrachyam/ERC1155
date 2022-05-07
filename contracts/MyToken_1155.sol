95f34a7d6abaf6d5faf49f0ed0f281b1993ead3a
Hrachya Mnatsakanyan <hmnatsakanya@vmware.com>
2022-05-07 11:38:07 +0400

    Added functionality to mind by ERC20 tokens

--- contracts/MyToken_1155.sol
+++ contracts/MyToken_1155.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -9,26 +10,51 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 contract MyToken_1155 is ERC1155Supply, Ownable, ReentrancyGuard {
 
     uint public tokenPriceByEth;
+    uint public tokenPriceByUsdc;
     uint public amountLimitOfToken;
     uint private _setCount;
     string private _tokenBaseUri;
 
-    constructor(string memory baseUri, uint priceByEth, uint setCount, uint amountLimit) 
+    constructor(string memory baseUri, uint priceByEth, uint priceByUsdc, uint setCount, uint amountLimit) 
         ERC1155("") 
     {
         _tokenBaseUri = baseUri;
         _setCount = setCount;
         amountLimitOfToken = amountLimit; 
         tokenPriceByEth = priceByEth;
+        tokenPriceByUsdc = priceByUsdc;
     }
+
+    event MintByEther(address indexed sender, uint indexed tokenId, uint256 amount);
+    event MintByErc20(address indexed sender, uint indexed tokenId, uint256 amount);
 
     function mintByEth(uint256 tokenId, uint256 amount) 
         external payable nonReentrant 
     {
         require(tokenId >= 1 && tokenId <= _setCount, "Incorrect Token ID");
         require(totalSupply(tokenId) + amount <= amountLimitOfToken, "Total amount must be less than limit!");
-        require(msg.value >= tokenPriceByEth * amount, "Token price is less!");
+        require(msg.value >= tokenPriceByEth * amount, "Insufficient Eth to mint token!");
+        emit MintByEther(msg.sender, tokenId, amount);
         _mint(msg.sender, tokenId, amount, "");
+    }
+
+    function mintByErc20(address erc20, uint256 tokenId, uint256 amount) 
+        external payable nonReentrant 
+    {
+        require(tokenId >= 1 && tokenId <= _setCount, "Incorrect Token ID!");
+        require(totalSupply(tokenId) + amount <= amountLimitOfToken, "Total amount must be less than limit!");
+        IERC20 erc20Token = IERC20(erc20);
+        uint totalPrice = tokenPriceByUsdc * amount;
+        require(erc20Token.balanceOf(msg.sender) >= totalPrice, "Insufficient balance to mint token!");
+        require(erc20Token.allowance(msg.sender, address(this)) >= totalPrice, "Must be approved before transfering!");
+        bool success = erc20Token.transferFrom(msg.sender, address(this), totalPrice);
+        require(success, "Failed to transfer!");
+        emit MintByErc20(msg.sender, tokenId, amount);
+        _mint(msg.sender, tokenId, amount, "");
+    }
+
+    function balance() external view returns(uint) {
+        return address(this).balance;
     }
 
     function uri(uint256 tokenId) public view override returns (string memory) {
